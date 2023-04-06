653d614e1c9bc303bd2fb4c0b72a3e781535f403
hieu521 <nguyenhuuhieu304@gmail.com>
2023-04-06 08:50:13 +0700

    create delete Books

---
+++ Models/FileModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookShopping.Models;
+
+public class FileModel
+{
+    [Key]
+    private int Id { get; set; }
+    public string Name { get; set; }
+    public byte[] Data { get; set; }
+}
---
+++ ViewModels/UpsetBookViewModel.cs
@@ -0,0 +1,12 @@
+﻿using BookShopping.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+
+namespace BookShopping.Areas.Authenticated.ViewMode;
+
+
+public class UpsetBookViewModel 
+{
+    public Book Book { get; set; }
+    public List<SelectListItem> Categories { get; set; }
+}
